56ac7954d7faaf96f27bfe3a7b67ee2d8ceca125
Ngandoll07 <22dh112324@st.huflit.edu.vn>
2024-03-04 11:16:45 +0700

    first commit

---
+++ app/src/main/java/com/example/music/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edtUsername,edtPass;
+    Button btnLogin,btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable icError=getResources().getDrawable(R.drawable.ic_error);
+                icError.setBounds(0,0,icError.getIntrinsicWidth(),icError.getIntrinsicHeight());
+                String username=edtUsername.getText().toString().trim();
+                String password=edtPass.getText().toString().trim();
+                if (username.isEmpty()){
+                    edtUsername.setCompoundDrawables(null,null,icError,null);
+                    edtUsername.setError("Vui lòng nhập tên",icError);
+                }
+                if (password.isEmpty()){
+                    edtPass.setCompoundDrawables(null,null,icError,null);
+                    edtPass.setError("Vui lòng nhập mật khẩu",icError);
+                }
+                if (!username.isEmpty()&&!password.isEmpty()){
+                    edtUsername.setCompoundDrawables(null,null,null,null);
+                    edtPass.setCompoundDrawables(null,null,null,null);
+                    Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("username",username);
+                    intent.putExtra("password",password);
+                    startActivity(intent);
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void addControls() {
+        edtUsername=findViewById(R.id.edtUsername);
+        edtPass=findViewById(R.id.edtPass);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnCancel=findViewById(R.id.btnCancel);
+    }
+}
---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.music;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.music.fragment_menu.AccountFragment;
+import com.example.music.fragment_menu.ViewPagerAdapter;
+import com.example.music.tab_music.MusicFiles;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 1;
+    public static ArrayList<MusicFiles> musicFiles;
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        permission();
+    }
+//allow
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE)
+        {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                musicFiles = getAudio(this);
+                MviewPager();
+            }
+            else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_MEDIA_AUDIO},REQUEST_CODE);}
+        }
+    }
+
+    private void permission() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_MEDIA_AUDIO)
+        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_MEDIA_AUDIO},REQUEST_CODE);
+        }
+        else {
+            musicFiles = getAudio(this);
+            MviewPager();
+        }
+    }
+//botNav
+    private void MviewPager() {
+        viewPager =findViewById(R.id.viewpaper);
+        bottomNavigationView = findViewById(R.id.BottomNAV);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_music).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_account).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.menu_music) {
+                    viewPager.setCurrentItem(0);
+                }
+                else if(item.getItemId()==R.id.menu_account) {
+                    viewPager.setCurrentItem(1);
+                }
+                return true;
+            }
+        });
+    }
+    //Song
+    public static ArrayList<MusicFiles> getAudio(Context context)
+    {
+        ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+        Uri url = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+        Cursor cursor = context.getContentResolver().query(url,projection,
+                null,null,null);
+        if(cursor!=null){
+            while(cursor.moveToNext())
+            {
+                String album = cursor.getString(0);
+                String title = cursor.getString(1);
+                String duration = cursor.getString(2);
+                String path = cursor.getString(3);
+                String artist = cursor.getString(4);
+                MusicFiles musicFiles= new MusicFiles(path,title,artist,album,duration);
+                Log.e("Path: "+ path,"Album: "+album);
+                tempAudioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return tempAudioList;
+    }
+
+
+
+}
